874eb2b046853f274e13549ab7473622d7407229
Matthew Carter <m@ahungry.com>
2018-10-30 23:18:01 -0400

    Add music, add particle effects

--- dodge-the-creeps/Main.gd
+++ dodge-the-creeps/Main.gd
@@ -18,11 +18,14 @@ func _ready():
 #	pass
 
 func game_over():
+	$Music.stop()
+	$GameOver.play()
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
 
 func new_game():
+	$Music.play()
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
